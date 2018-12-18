7446bf32f4b82a80e60d1324cd81f019ee647172
mariapedrosal <45463334+mariapedrosal@users.noreply.github.com>
2018-12-18 11:39:40 +0100

    Add files via upload

--- ML.py
+++ ML.py
@@ -11,15 +11,19 @@ from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 ##Importar los metodos que vamos a utilizar
-#
+from sklearn.metrics import classification_report
 #from pandas.plotting import scatter_matrix
 #import matplotlib.pyplot as plt
 #from sklearn import model_selection
-#from sklearn.metrics import classification_report
+from sklearn.ensemble import RandomForestClassifier
 #from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 #from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import roc_curve, auc
+from sklearn.neural_network import MLPClassifier
 #from sklearn.neighbors import KNeighborsClassifier
 #from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 #from sklearn.naive_bayes import GaussianNB
@@ -79,7 +83,6 @@ columnas = list(train.columns.values)
 seleccionadas = SelectKBest(f_classif, k=k).fit(train, ytrain['cancer'])
 atrib = seleccionadas.get_support()
 atributos1 = [columnas[i] for i in list(atrib.nonzero()[0])]
-atributos1
 
 #Eliminacion recursiba de atributos
 modelo = ExtraTreesClassifier()
@@ -87,7 +90,6 @@ era = RFE(modelo, 100)  # número de atributos a seleccionar
 era = era.fit(train, ytrain['cancer'])
 atrib2 = era.support_
 atributos2 = [columnas[i] for i in list(atrib2.nonzero()[0])]
-atributos2
 
 #Se combinan los tributos elegidos en una lista
 atribselec=list(set(atributos1)|set(atributos2))
@@ -118,33 +120,102 @@ test=pca.transform(testred)
 #max_attributes = len(list(ytrain))
 #depth_range = range(1, max_attributes + 1)
 
+#Buscar los mejores parametros por cross validation
+split_range=list(range(2,15))
+prof_range=list(range(2, 10))
 
+param_grid={'min_samples_split':split_range, 'max_depth':prof_range}
 clf_gini = DecisionTreeClassifier(criterion = 'gini', random_state=100)
-clf_gini.fit(train, ytrain)
+grid_dt=GridSearchCV(clf_gini, param_grid, scoring='accuracy')
+grid_dt.fit(train, ytrain['cancer'])
+print(grid_dt.best_score_)
+print(grid_dt.best_params_)
 
-ypred=clf_gini.predict(test)
+mejor_clf_gini=grid_dt.best_estimator_
+
+
+ypred=mejor_clf_gini.predict(test)
+print(mejor_clf_gini.predict_proba(test))
+print(pd.crosstab(ytest['cancer'],ypred, rownames=['Actual diagnosis'], colnames=['Predicted diagnosis']))
+ypred_t=mejor_clf_gini.predict(train)
+print(pd.crosstab(ytrain['cancer'],ypred_t, rownames=['Actual diagnosis'], colnames=['Predicted diagnosis']))
+print(classification_report(ytest['cancer'], ypred))
+false_positive_rate, true_positive_rate, thresholds = roc_curve(ytest['cancer'], ypred)
+roc_auc = auc(false_positive_rate, true_positive_rate)
+roc_auc
+#Visualización
+#from sklearn.externals.six import StringIO  
+#from IPython.display import Image  
+#from sklearn.tree import export_graphviz
+#import graphviz
+#dot_data = tree.export_graphviz(mejor_clf_gini, out_file=None, filled=True, rounded=True, special_characters=True)  
+#graph = graphviz.Source(dot_data)  
+#graph
 
 #Random forest
+# Number of trees in random forest
+n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
+# Number of features to consider at every split
+max_features = ['auto', 'sqrt']
+# Maximum number of levels in tree
+max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+max_depth.append(None)
+# Minimum number of samples required to split a node
+min_samples_split = [2, 5, 10]
+# Minimum number of samples required at each leaf node
+min_samples_leaf = [1, 2, 4]
+# Method of selecting samples for training each tree
+bootstrap = [True, False]
+# Create the random grid
+random_grid = {'n_estimators': n_estimators,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,
+               'bootstrap': bootstrap}
+
+rf_clf=RandomForestClassifier()
+rf_random = RandomizedSearchCV(estimator = rf_clf, param_distributions = random_grid, n_iter = 10, cv = 3, verbose=2, random_state=100, n_jobs = -1)
+rf_random.fit(train, ytrain['cancer'])
 
 
 
+print(rf_random.best_params_)
+
+param_grid = {
+    'bootstrap': [True],
+    'max_depth': [None],
+    'max_features': ['auto'],
+    'min_samples_leaf': [1, 2, 3],
+    'min_samples_split': [2, 4, 6],
+    'n_estimators': [500, 1000, 2000]
+}
+
+grid_rf=GridSearchCV(rf_clf, param_grid, scoring='accuracy', cv=3)
+grid_rf.fit(train, ytrain['cancer'])
+print(grid_rf.best_score_)
+print(grid_rf.best_params_)
+
+mejor_rf_clf=grid_rf.best_estimator_
 
 
-#Super Vector Machine
+ypred_rf=mejor_rf_clf.predict(test)
+print(mejor_rf_clf.predict_proba(test))
+print(pd.crosstab(ytest['cancer'],ypred_rf, rownames=['Actual diagnosis'], colnames=['Predicted diagnosis']))
+ypred_t_rf=mejor_rf_clf.predict(train)
+print(pd.crosstab(ytrain['cancer'],ypred_t_rf, rownames=['Actual diagnosis'], colnames=['Predicted diagnosis']))
+print(classification_report(ytest['cancer'], ypred_rf))
 
+false_positive_rate, true_positive_rate, thresholds = roc_curve(ytest['cancer'], ypred_rf)
+roc_auc_rf = auc(false_positive_rate, true_positive_rate)
+roc_auc_rf
 
-
-
-
-
-
-#for depth in depth_range:
-#    fold_accuracy = []
-#    tree_model = tree.DecisionTreeClassifier(criterion='gini',
-#                                             min_samples_split=20,
-#                                             min_samples_leaf=5,
-#                                             max_depth = depth,
-#                                             class_weight={2:1})
-#    for train_fold, valid_fold in cv.split():
-#        f_train = artists_encoded.loc[train_fold] 
-#        f_valid = artists_encoded.loc[valid_fold]
+#Neural network
+mlp = MLPClassifier(hidden_layer_sizes=(30,30,30))
+mlp.fit(train, ytrain['cancer'])
+nn_pred=mlp.predict(test)
+print(pd.crosstab(ytest['cancer'],nn_pred, rownames=['Actual diagnosis'], colnames=['Predicted diagnosis']))
+print(classification_report(ytest['cancer'], nn_pred))
+false_positive_rate, true_positive_rate, thresholds = roc_curve(ytest['cancer'], nn_pred)
+roc_auc = auc(false_positive_rate, true_positive_rate)
+roc_auc
